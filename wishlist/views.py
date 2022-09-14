292166f045832e7f47e79b5c93bf5868060a9820
khansajovita <khansajovita@gmail.com>
2022-09-15 01:35:41 +0700

    Update Lab2 Khansa Jovita

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,9 @@
+import json
+from xml.etree.ElementTree import XML
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.http import HttpResponse
+from django.core import serializers
 ...
 
 # Create your views here.
@@ -10,3 +14,18 @@ def show_wishlist(request):
         'nama': 'Khansa Jovita'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    if id == 1:
+        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    if id == 3:
+        return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
